58899f7179e41c4814b8fc02b7c6d77c70212fc2
Portero <eportero421@gmail.com>
2024-05-20 10:07:05 -0300

    refactor: Add new utilities folder

---
+++ js/utilities/about.mjs
@@ -0,0 +1,5 @@
+export const ABOUT = {
+
+    description: '\n\nAbout me: Im from Catamarca province located in the northern region of Argentina, passionate about programming, cinema, sports, and video games.'
+
+}

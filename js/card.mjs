8cd87e3d16da08c0714aa107e06172e6ad173175
Portero <eportero421@gmail.com>
2024-05-20 10:06:05 -0300

    chore: Update card.mjs
    Update the business card with new information

--- js/card.mjs
+++ js/card.mjs
@@ -3,17 +3,18 @@
 
 import boxen from "boxen";
 import chalk from "chalk";
+import { ABOUT } from "./utilities/about.mjs";
 
 const boxenOptions = {
     padding: 1,
     width: 70,
-    title: "Exequiel Portero",
+    title: "Exequiel Portero / @Portero_DEV",
     titleAlignment: "center",
     borderStyle: "double",
     borderColor: "#ffb626",
 };
 
-const intro = chalk.bold("I'm Frontend Developer and Sofware Developer Technician.\n\nFind me on the internet:\n\n");
+const intro = chalk.bold("I'm Frontend Developer and Sofware Developer Technician.\n\nFollow me in:\n\n");
 
 const links = [
     {
@@ -21,15 +22,19 @@ const links = [
         url: "https://www.linkedin.com/in/exequiel-portero"
     },
     {
+        name: chalk.hex("#fff").bgHex("#FF0000")("Youtube:"),
+        url: " https://www.youtube.com/@PorteroDEV_"
+    },
+    {
         name: chalk.hex("#fff").bgHex("#6e5494")("GitHub:"),
         url: "  https://github.com/Portero20",
     },
     {
         name: chalk.hex("#fff").bgHex("#ffcf40")("Card:"),
-        url: "    npx porterocard",
+        url: "   `npx` porterocard",
     },
 ];
 
 const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
 
-console.log(boxen(intro + linkList, boxenOptions));
+console.log(boxen(intro + linkList + ABOUT.description, boxenOptions));
